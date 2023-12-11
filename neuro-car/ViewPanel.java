cd033d935ed968509954f6af33e15c291cece9a5
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-12-11 14:47:56 +0100

    added current neuro-car project

---
+++ neuro-car/ViewPanel.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class ViewPanel extends JPanel {
+    
+    private Color grassColor = new Color(0, 150, 0);
+    private Track track;
+
+    ViewPanel(int width, int height) {
+        this.setPreferredSize(new Dimension(width, height));
+        this.track = new Track();
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D graph = (Graphics2D) g;
+        graph.setColor(this.grassColor);
+        graph.fillRect(0, 0, 1080, 720);
+        track.paint(graph);
+    }
+}
